abd39a7ff1ab06b09194883dacb718b727a61975
Daniil <daniil_drokin06@mail.ru>
2022-04-01 20:02:51 +0300

    Version 0.4

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import telebot
 import sqlite3
+import telebot
 from telebot import types
-token = 'token'
+token = '5282834057:AAGKZQR5A4HWvcE-oRr15Ucv_OPo2KCVdRA'
 bot = telebot.TeleBot(token)
 
 
@@ -9,11 +9,35 @@ bot = telebot.TeleBot(token)
 def start_message(message):
     bot.send_message(message.chat.id, 'Привет')
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton("ОГЭ")
-    item2 = types.KeyboardButton("ЕГЭ")
+    item1 = types.KeyboardButton("/oge")
+    item2 = types.KeyboardButton("/ege")
     markup.add(item1)
     markup.add(item2)
     bot.send_message(message.chat.id, 'Выберите, к чему вы хотите готовиться:', reply_markup=markup)
+
+
+@bot.message_handler(commands=['oge'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Выберите предмет:')
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("Математика")
+    item2 = types.KeyboardButton("Русский язык")
+    markup.add(item1)
+    markup.add(item2)
+    bot.send_message(message.chat.id, 'тест1', reply_markup=markup)
+
+
+@bot.message_handler(commands=['ege'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Выберите предмет:')
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("Профильная математика")
+    item2 = types.KeyboardButton("Базовая математика")
+    item3 = types.KeyboardButton("Русский язык")
+    markup.add(item1)
+    markup.add(item2)
+    markup.add(item3)
+    bot.send_message(message.chat.id, 'тест2', reply_markup=markup)
 
 
 @bot.message_handler(content_types='text')
@@ -31,7 +55,9 @@ def message_reply(message):
             result = cur.execute("""SELECT task, answer FROM rus_yaz
              WHERE id IN (SELECT id FROM rus_yaz ORDER BY RANDOM() LIMIT 1)""").fetchall()
             for elem in result:
+                print(elem[1])
                 bot.send_message(message.chat.id, f'{elem[1]}')
+
             con.close()
 
     elif message.text.lower() == "егэ":
@@ -43,8 +69,6 @@ def message_reply(message):
         markup.add(item2)
         markup.add(item3)
         bot.send_message(message.chat.id, 'Выберите предмет:', reply_markup=markup)
-    else:
-        bot.send_message(message.chat.id, "Я вас не понимаю, повторите еще раз")
 
 
 bot.infinity_polling()
