5ca6516434527bbbb6b96c22242f2c2aa2f9a29c
Daniil <daniil_drokin06@mail.ru>
2022-04-02 21:23:53 +0300

    Version 0.7

--- main.py
+++ main.py
@@ -48,10 +48,27 @@ def message_reply(message):
         result = cur.execute("""SELECT task, answer FROM rus_yaz
             WHERE id IN (SELECT id FROM rus_yaz ORDER BY RANDOM() LIMIT 1)""").fetchall()
         for elem in result:
-            print(elem[1])
+            global correct
+            correct = elem[1]
             bot.send_photo(message.chat.id, photo=elem[0])
+            bot.register_next_step_handler(message, get_answer)
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1 = types.KeyboardButton("Попробовать еще")
+        item2 = types.KeyboardButton("Отказаться")
+        markup.add(item1)
+        markup.add(item2)
+        bot.send_message(message.chat.id, 'Хотите попробовать еще?', reply_markup=markup)
 
         con.close()
+
+
+def get_answer(message):
+    global answer
+    answer = message.text
+    if answer.lower() == correct:
+        bot.send_message(message.chat.id, 'Правильный ответ!')
+    else:
+        bot.send_message(message.chat.id, 'Ошибка!')
 
 
 bot.infinity_polling()
