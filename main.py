06d7ca2645ac27e0d1c13fcf49cedbf92083f537
Daniil <daniil_drokin06@mail.ru>
2022-04-02 21:56:57 +0300

    Version 0.8

--- main.py
+++ main.py
@@ -52,23 +52,37 @@ def message_reply(message):
             correct = elem[1]
             bot.send_photo(message.chat.id, photo=elem[0])
             bot.register_next_step_handler(message, get_answer)
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        item1 = types.KeyboardButton("Попробовать еще")
-        item2 = types.KeyboardButton("Отказаться")
-        markup.add(item1)
-        markup.add(item2)
-        bot.send_message(message.chat.id, 'Хотите попробовать еще?', reply_markup=markup)
-
         con.close()
 
 
 def get_answer(message):
     global answer
     answer = message.text
-    if answer.lower() == correct:
+    if ''.join(answer.lower().split()) == correct:
         bot.send_message(message.chat.id, 'Правильный ответ!')
     else:
         bot.send_message(message.chat.id, 'Ошибка!')
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("Попробовать еще")
+    item2 = types.KeyboardButton("Отказаться")
+    markup.add(item1)
+    markup.add(item2)
+    bot.send_message(message.chat.id, 'Хотите попробовать еще?', reply_markup=markup)
+    bot.register_next_step_handler(message, return0)
+
+
+def return0(message):
+    com = message.text
+    if com.lower() == 'попробовать еще':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item = types.KeyboardButton("/oge")
+        markup.add(item)
+        bot.send_message(message.chat.id, 'Начать с начала', reply_markup=markup)
+    else:
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item = types.KeyboardButton("/start")
+        markup.add(item)
+        bot.send_message(message.chat.id, 'До скорых встреч!', reply_markup=markup)
 
 
 bot.infinity_polling()
