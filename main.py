f52541c98b6feee607efd1d1ace3d432497ab344
Andrew <alegenda05@gmail.com>
2022-04-06 19:26:47 +0300

    Version 0.14.3

--- main.py
+++ main.py
@@ -4,15 +4,36 @@ from telebot import types
 token = '5282834057:AAGKZQR5A4HWvcE-oRr15Ucv_OPo2KCVdRA'
 bot = telebot.TeleBot(token)
 
+#conn1 = sqlite3.connect('db/datab.db', check_same_thread=False)
+#cursor1 = conn1.cursor()
+
+
+#def db_table_val(user_id: int, user_name: str, user_surname: str, username: str, ranswer: int):
+    #cursor1.execute('INSERT INTO users (user_id, user_name, user_surname, username, ranswer) VALUES (?, ?, ?, ?, ?)', (user_id, user_name, user_surname, username, ranswer))
+    #conn1.commit()
+
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
+    #global r
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1 = types.KeyboardButton("/oge")
     item2 = types.KeyboardButton("/ege")
     markup.add(item1)
     markup.add(item2)
     bot.send_message(message.chat.id, 'Выберите экзамен:', reply_markup=markup)
+    #us_id = message.from_user.id
+    #us_name = message.from_user.first_name
+    #us_sname = message.from_user.last_name
+    #username = message.from_user.username
+    #db_table_val(user_id=us_id, user_name=us_name, user_surname=us_sname, username=username, ranswer=0)
+    #con = sqlite3.connect('db/datab.db')
+    #cur = con.cursor()
+    #result = cur.execute(f"""SELECT ranswer FROM users
+            #WHERE user_id = {message.from_user.id}""").fetchall()
+    #for elem in result:
+        #r = elem[0]
+    #con.close()
 
 
 @bot.message_handler(commands=['oge'])
@@ -22,10 +43,12 @@ def start_message(message):
     item2 = types.KeyboardButton("Русский язык(ОГЭ)")
     item3 = types.KeyboardButton("Физика(ОГЭ)")
     item4 = types.KeyboardButton("Информатика(ОГЭ)")
+    item5 = types.KeyboardButton("Химия(ОГЭ)")
     markup.add(item1)
     markup.add(item2)
     markup.add(item3)
     markup.add(item4)
+    markup.add(item5)
     bot.send_message(message.chat.id, 'Выберите предмет:', reply_markup=markup)
 
 
@@ -58,6 +81,7 @@ def message_reply(message):
             bot.send_message(message.from_user.id, 'Ваш ответ:', reply_markup=telebot.types.ReplyKeyboardRemove())
             bot.register_next_step_handler(message, get_answer)
         con.close()
+
     if message.text.lower() == "русский язык(егэ)":
         con = sqlite3.connect('db/ege.db')
         cur = con.cursor()
@@ -136,12 +160,33 @@ def message_reply(message):
             bot.send_message(message.from_user.id, 'Ваш ответ:', reply_markup=telebot.types.ReplyKeyboardRemove())
             bot.register_next_step_handler(message, get_answer)
         con.close()
+    if message.text.lower() == "химия(огэ)":
+        con = sqlite3.connect('db/oge.db')
+        cur = con.cursor()
+        name = "Химия(ОГЭ)"
+        result = cur.execute("""SELECT task, answer FROM him
+             WHERE id IN (SELECT id FROM him ORDER BY RANDOM() LIMIT 1)""").fetchall()
+        for elem in result:
+            print(elem[1])
+            correct = elem[1]
+            bot.send_photo(message.chat.id, photo=elem[0])
+            bot.send_message(message.from_user.id, 'Ваш ответ:', reply_markup=telebot.types.ReplyKeyboardRemove())
+            bot.register_next_step_handler(message, get_answer)
+        con.close()
 
 
 def get_answer(message):
     global answer
+    #global r
     answer = message.text
     if ''.join(answer.lower().split()) == correct:
+        #con = sqlite3.connect('db/datab.db')
+        #cur = con.cursor()
+        #result = cur.execute(f"""UPDATE users
+        #SET ranswer = {r}
+        #WHERE user_id = {message.from_user.id}""").fetchall()
+        #con.commit()
+        #con.close()
         bot.send_message(message.chat.id, 'Правильный ответ!')
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         item1 = types.KeyboardButton("Попробовать еще")
